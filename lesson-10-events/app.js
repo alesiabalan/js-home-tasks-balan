f86f9716444db10c9aee9d9f038085e32f2f0b48
alesiabalan <alesia.laktionova@gmail.com>
2022-03-19 18:08:47 +0300

    fix all js code

--- lesson-10-events/app.js
+++ lesson-10-events/app.js
@@ -1,38 +1,140 @@
-function dynamicForm(arr, name) {
-    var elem;
-    for (var i = 0; i < arr.length; i++){
-        elem = document.write('<div style="display: flex; flex-direction: row"><label style="display: block; width: 200px">' + arr[i].label + '</label>' + arr[i].element+ '</div>');
+'use strict';
+(function () {
+    function makeForm(form, arr) {
+        arr.map(createForm);
+
+        function createForm(arr) {
+            if (arr.element === 'textline') {
+                return form.appendChild(createText(arr));
+            } else if (arr.element === 'select') {
+                return form.appendChild(createSelect(arr));
+            } else if (arr.element === 'radio') {
+                return form.appendChild(createRadio(arr));
+            } else if (arr.element === 'checkbox') {
+                return form.appendChild(createCheckbox(arr));
+            } else if (arr.element === 'textarea') {
+                return form.appendChild(createTextarea(arr));
+            } else if (arr.element === 'submit') {
+                return form.appendChild(createSubmit(arr));
+            };
+        };
+
+    }
+
+    function createLabel(name, caption) {
+        var label = document.createElement('label');
+        label.for = name;
+        label.innerHTML = caption;
+        return label;
     };
-    name.append(elem);
-    var button = document.write('<div><button>Опубликовать</button></div>');
-    name.append(button);
-}
-var arr = [
-    { label: 'Разработчики:', element: '<input style="width: 450px">'},
-    { label: 'Название сайта:', element: '<input style="width: 450px">'},
-    { label: 'URL сайта:', element: '<input style="width: 300px">'},
-    { label: 'Дата запуска сайта:', element: '<input style="width: 80px">'},
-    { label: 'Посетителей в сутки:', element: '<input style="width: 80px">'},
-    { label: 'E-mail для связи:', element: '<input style="width: 200px">'},
-    { label: 'Рубрика каталога:', element: '<select style="width: 200px">\n' +
-                                           '<option value="value1" selected>Бытовая техника</option>\n' +
-                                           '<option value="value2">Еда</option>\n' +
-                                           '</select>'
-    },
-    { label: 'Размещение:', element: '<div>\n' +
-                                     '<input type="radio" id="payChoice1">\n' +
-                                     '<label for="payChoice1">бесплатное</label>\n' +
-                                     '<input type="radio" id="payChoice2">\n' +
-                                     '<label for="payChoice2">платное</label>\n' +
-                                     '<input type="radio" id="payChoice3">\n' +
-                                     '<label for="payChoice3">VIP</label>\n' +
-                                     '</div>'
-    },
-    { label: 'Разрешить отзывы:', element: '<input type="checkbox" checked>'},
-    { label: 'Описание сайта:', element: '<textarea style="width: 600px; height: 150px"></textarea>'}
-];
-var name = document.getElementById('nameOfForm');
-dynamicForm(arr, name);
 
+    function createInput(name, element) {
+        var input = document.createElement('input');
+        input.name = name;
+        input.type = element;
+        return input;
+    };
 
+    function createText(arr) {
+        var caption = arr.label;
+        var name = arr.name;
+        var element = arr.element;
 
+        var div = document.createElement('div');
+        div.appendChild(createLabel(name, caption));
+        div.appendChild(createInput(name, element));
+
+        return div;
+    };
+
+    function createSelect(arr) {
+        var caption = arr.label;
+        var name = arr.name;
+        var options = arr.items;
+
+        var div = document.createElement('div');
+        var label = createLabel(name, caption);
+        var select = document.createElement('select');
+        select.name = name;
+
+        div.appendChild(label);
+        div.appendChild(select);
+        for (var i = 0; i < options.length; i++) {
+            var option = document.createElement('option');
+            option.innerHTML = options[i];
+            select.appendChild(option);
+        }
+        return div;
+    };
+
+    function createRadio(arr) {
+        var caption = arr.label;
+        var element = arr.element;
+        var name = arr.name;
+        var options = arr.items;
+
+        var div = document.createElement('div');
+        div.appendChild(createLabel(name, caption));
+        for (var i = 0; i < options.length; i++) {
+            div.appendChild(createInput(name, element));
+            div.appendChild(document.createTextNode(options[i]));
+        }
+        return div;
+    };
+
+    function createCheckbox(arr) {
+        var caption = arr.label;
+        var element = arr.element;
+        var name = arr.name;
+
+        var div = document.createElement('div');
+        div.appendChild(createLabel(name, caption));
+        div.appendChild(createInput(name, element));
+
+        return div;
+    };
+
+    function createTextarea(arr) {
+        var caption = arr.label;
+        var name = arr.name;
+        var cols = arr.cols;
+        var rows = arr.rows;
+
+        var div = document.createElement('div');
+        div.appendChild(createLabel(name, caption));
+        var textarea = document.createElement('textarea');
+        textarea.cols = cols;
+        textarea.rows = rows;
+        div.appendChild(textarea);
+
+        return div;
+    };
+
+    function createSubmit(arr) {
+        var caption = arr.label;
+        var element = arr.element;
+        var name = arr.name;
+
+        var submit = createInput(name, element);
+        submit.value = caption;
+
+        return submit;
+    };
+
+    var form = document.forms['nameOfForm'];
+    var arr = [
+        {label: 'Разработчики:', element: 'textline', name: 'developers'},
+        {label: 'Название сайта:', element: 'textline', name: 'nameOfSite'},
+        {label: 'URL сайта:', element: 'textline', name: 'URL'},
+        {label: 'Дата запуска сайта:', element: 'textline', name: 'datePlaySite'},
+        {label: 'Посетителей в сутки:', element: 'textline', name: 'countDay'},
+        {label: 'E-mail для связи:', element: 'textline', name: 'emailConnect'},
+        {label: 'Рубрика каталога:', element: 'select', name: 'rubric', items: ['бытовая техника', 'еда', 'одежда']},
+        {label: 'Размещение:', element: 'radio', name: 'placing', items: ['бесплатное', 'платное', 'VIP']},
+        {label: 'Разрешить отзывы:', element: 'checkbox', name: 'permission'},
+        {label: 'Описание сайта:', element: 'textarea', name: 'description', cols: '30', rows: '8'},
+        {label: 'Опубликовать', element: 'submit', name: 'publish'}
+    ];
+
+    makeForm(form, arr);
+})();
